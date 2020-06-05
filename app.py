e66fe694af26655cb3f80ddce4a547b872ef16f4
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 07:54:16 +0000

    small fix for ordering of tickets - removing the prefix changed the length of text, so it now replaces the prefix with *'s as it finds them

--- app.py
+++ app.py
@@ -54,17 +54,17 @@ def handleTickets(text):
     while True:
         if ('dev-' in text):
             orderedTicketLinks[text.index('dev-')] = ticketLink(text, 'DEV-')
-            # This line removes the first occurance of 'dev-' in the text 
-            text = text[:text.index('dev-')]+text[text.index('dev-')+4:]
+            # This line replaces the first occurance of 'dev-' in the text with ****
+            text = text[:text.index('dev-')]+"****"+text[text.index('dev-')+4:]
         elif ('pp-' in text):
             orderedTicketLinks[text.index('pp-')] = ticketLink(text, 'PP-')
-            text = text[:text.index('pp-')]+text[text.index('pp-')+3:]
+            text = text[:text.index('pp-')]+"***"text[text.index('pp-')+3:]
         elif ('sc-' in text):
             orderedTicketLinks[text.index('sc-')] = ticketLink(text, 'SC-')
-            text = text[:text.index('sc-')]+text[text.index('sc-')+3:]
+            text = text[:text.index('sc-')]+"***"text[text.index('sc-')+3:]
         elif ('asiaqnt-' in text):
             orderedTicketLinks[text.index('asiaqnt-')] = ticketLink(text, 'ASIAQNT-')
-            text = text[:text.index('asiaqnt-')]+text[text.index('asiaqnt-')+8:]
+            text = text[:text.index('asiaqnt-')]+"********"text[text.index('asiaqnt-')+8:]
         else:
             break
     
